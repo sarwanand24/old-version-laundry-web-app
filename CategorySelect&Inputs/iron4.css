8dec0fb6b3860b0aa180d65597944bf7fef3a411
Nikhil Dhamgay <nikhildhamgay200424@gmail.com>
2023-08-07 23:30:22 +0530

    Create iron4.css

---
+++ CategorySelect&Inputs/iron4.css
@@ -0,0 +1,339 @@
+
+document.getElementById("openSidebarMenu").addEventListener("click", function(){
+
+var hidden = document.getElementById("hidden");
+hidden.style.display = openSidebarMenu.checked? "none" : "block";
+
+})
+
+
+
+           /* Sweat Shirt */
+      
+const d2 = document.getElementById("d2");
+    const d1 = document.getElementById("d1");
+    const p1 = document.getElementById("p1");
+    var count = 0;
+    const total = document.getElementById("total");
+    var total1 = 0;
+    p1.innerHTML = count;
+    const handleIncrement = () => {
+        count++;
+        total1 = 15*count;
+        p1.innerHTML = count;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement = () => {
+    if(count>0)
+        count--;
+        total1 = 15*count;
+    p1.innerHTML = count;
+    total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d2.addEventListener("click", handleIncrement);
+    d1.addEventListener("click", handleDecrement);
+    
+
+         /* Sweater Half */
+         const d4 = document.getElementById("d4");
+    const d3 = document.getElementById("d3");
+    const p2 = document.getElementById("p2");
+    var count2 = 0;
+    var total2 = 0;
+    p2.innerHTML = count2;
+    const handleIncrement2 = () => {
+        count2++;
+        total2 = 20*count2;
+        p2.innerHTML = count2;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement2 = () => {
+    if(count2>0)
+        count2--;
+        total2 = 20*count2;
+    p2.innerHTML = count2;
+    total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d4.addEventListener("click", handleIncrement2);
+    d3.addEventListener("click", handleDecrement2);
+    
+              /* Waist Coat */
+
+ const d6 = document.getElementById("d6");
+    const d5 = document.getElementById("d5");
+    const p3 = document.getElementById("p3");
+    var count3 = 0;
+    var total3 = 0;
+    p3.innerHTML = count3;
+    const handleIncrement3 = () => {
+        count3++;
+        total3 = 20*count3;
+        p3.innerHTML = count3;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement3 = () => {
+    if(count3>0)
+        count3--;
+        total3 = 20*count3;
+    p3.innerHTML = count3;
+    total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d6.addEventListener("click", handleIncrement3);
+    d5.addEventListener("click", handleDecrement3);
+    
+               /* Coat */
+
+  const d8 = document.getElementById("d8");
+    const d7 = document.getElementById("d7");
+    const p4 = document.getElementById("p4");
+    var count4 = 0;
+    var total4 = 0;
+    p4.innerHTML = count4;
+    const handleIncrement4 = () => {
+        count4++;
+        total4 = 15*count4;
+        p4.innerHTML = count4;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement4 = () => {
+    if(count4>0)
+        count4--;
+        total4 = 15*count4;
+    p4.innerHTML = count4;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d8.addEventListener("click", handleIncrement4);
+    d7.addEventListener("click", handleDecrement4);
+    
+               /* Over Coat */
+
+  const d10 = document.getElementById("d10");
+    const d9 = document.getElementById("d9");
+    const p5 = document.getElementById("p5");
+    var count5 = 0;
+    var total5 = 0;
+    p5.innerHTML = count5;
+    const handleIncrement5 = () => {
+        count5++;
+        total5 = 25*count5;
+        p5.innerHTML = count5;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement5 = () => {
+    if(count5>0)
+        count5--;
+        total5 = 25*count5;
+    p5.innerHTML = count5;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d10.addEventListener("click", handleIncrement5);
+    d9.addEventListener("click", handleDecrement5);
+         
+               /* Jacket Half */
+
+               const d12 = document.getElementById("d12");
+    const d11 = document.getElementById("d11");
+    const p6 = document.getElementById("p6");
+    var count6 = 0;
+    var total6 = 0;
+    p6.innerHTML = count6;
+    const handleIncrement6 = () => {
+        count6++;
+        total6 = 20*count6;
+        p6.innerHTML = count6;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement6 = () => {
+    if(count6>0)
+        count6--;
+        total6 = 20*count6;
+    p6.innerHTML = count6;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d12.addEventListener("click", handleIncrement6);
+    d11.addEventListener("click", handleDecrement6);
+         
+               /* Jacket (Full) */
+
+               const d14 = document.getElementById("d14");
+    const d13 = document.getElementById("d13");
+    const p7 = document.getElementById("p7");
+    var count7 = 0;
+    var total7 = 0;
+    p7.innerHTML = count7;
+    const handleIncrement7 = () => {
+        count7++;
+        total7 = 50*count7;
+        p7.innerHTML = count7;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement7 = () => {
+    if(count7>0)
+        count7--;
+        total7 = 50*count7;
+    p7.innerHTML = count7;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d14.addEventListener("click", handleIncrement7);
+    d13.addEventListener("click", handleDecrement7);
+         
+                /* Shawl */
+
+                const d16 = document.getElementById("d16");
+    const d15 = document.getElementById("d15");
+    const p8 = document.getElementById("p8");
+    var count8 = 0;
+    var total8 = 0;
+    p8.innerHTML = count8;
+    const handleIncrement8 = () => {
+        count8++;
+        total8 = 20*count8;
+        p8.innerHTML = count8;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement8 = () => {
+    if(count8>0)
+        count8--;
+        total8 = 20*count8;
+    p8.innerHTML = count8;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10 ;
+    }
+     
+    d16.addEventListener("click", handleIncrement8);
+    d15.addEventListener("click", handleDecrement8);
+           
+             /* Shawl Fancy */
+
+const d18 = document.getElementById("d18");
+    const d17 = document.getElementById("d17");
+    const p9 = document.getElementById("p9");
+    var count9 = 0;
+    var total9 = 0;
+    p9.innerHTML = count9;
+    const handleIncrement9 = () => {
+        count9++;
+        total9 = 30*count9;
+        p9.innerHTML = count9;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement9 = () => {
+    if(count9>0)
+        count9--;
+        total9 = 30*count9;
+    p9.innerHTML = count9;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d18.addEventListener("click", handleIncrement9);
+    d17.addEventListener("click", handleDecrement9);
+           
+              /* Sweater (Full) */
+
+  const d20 = document.getElementById("d20");
+    const d19 = document.getElementById("d19");
+    const p10 = document.getElementById("p10");
+    var count10 = 0;
+    var total10 = 0;
+    p10.innerHTML = count10;
+    const handleIncrement10 = () => {
+        count10++;
+        total10 = 45*count10;
+        p10.innerHTML = count10;
+        total.innerHTML = total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+    
+    const handleDecrement10 = () => {
+    if(count10>0)
+        count10--;
+        total10 = 45*count10;
+    p10.innerHTML = count10;
+    total.innerHTML = total1 + total2 + total3 + total4  + total5 + total6 + total7 + total8 + total9 + total10;
+    }
+     
+    d20.addEventListener("click", handleIncrement10);
+    d19.addEventListener("click", handleDecrement10);
+           
+
+    document.getElementById("continue").addEventListener("click", function(){
+
+var sweat, sweater_half, waist, coat, over_coat, jacket_half, jacket_full, shawl, shawl_fancy, sweater_full , total_rate , total_proceed, sweat10, sweater_half10, waist10, coat10, over_coat10, jacket_half10, jacket_full10, shawl10, shawl_fancy10, sweater_full10 , total_proceed10;
+
+ sweat10 = sweat = document.getElementById("p1").innerText;
+ sweater_half10 = sweater_half = document.getElementById("p2").innerText;
+ waist10 = waist = document.getElementById("p3").innerText;
+coat10 = coat = document.getElementById("p4").innerText;
+over_coat10 = over_coat = document.getElementById("p5").innerText;
+jacket_half10 = jacket_half = document.getElementById("p6").innerText;
+jacket_full10 = jacket_full = document.getElementById("p7").innerText;
+shawl10 = shawl = document.getElementById("p8").innerText;
+shawl_fancy10 = shawl_fancy = document.getElementById("p9").innerText;
+sweater_full10 = sweater_full = document.getElementById("p10").innerText;
+total_proceed10 = total_proceed = total_rate = document.getElementById("total").innerText;
+
+localStorage.setItem("sweat1", sweat);
+localStorage.setItem("sweater_half1", sweater_half);
+localStorage.setItem("waist1", waist);
+localStorage.setItem("coat1", coat);
+localStorage.setItem("over_coat1", over_coat);
+localStorage.setItem("jacket_half1", jacket_half);
+localStorage.setItem("jacket_full1", jacket_full);
+localStorage.setItem("shawl1", shawl);
+localStorage.setItem("shawl_fancy1", shawl_fancy);
+localStorage.setItem("sweater_full1", sweater_full);
+localStorage.setItem("woolentotal_rate_iron1", total_rate);
+
+
+localStorage.setItem("sweat11", sweat10);
+localStorage.setItem("sweater_half11", sweater_half10);
+localStorage.setItem("waist11", waist10);
+localStorage.setItem("coat11", coat10);
+localStorage.setItem("over_coat11", over_coat10);
+localStorage.setItem("jacket_half11", jacket_half10);
+localStorage.setItem("jacket_full11", jacket_full10);
+localStorage.setItem("shawl11", shawl10);
+localStorage.setItem("shawl_fancy11", shawl_fancy10);
+localStorage.setItem("sweater_full11", sweater_full10);
+localStorage.setItem("woolentotal_rate_iron11", total_proceed10);
+
+
+
+if(total_proceed>100){
+   
+    var flagiron4 = 1;
+    sessionStorage.setItem("flagiron4", flagiron4);
+   
+
+    document.getElementById("warning").style.display="none";
+    var outsort4 = localStorage.getItem("insort4");
+
+if(outsort4 == 1){
+window.location ="schedule-pickup.html";
+}
+else{
+    window.location ="steam1.html";
+}
+
+}
+if(total_proceed<100){
+  document.getElementById("warning").style.display="block";
+}
+
+
+
+    })
